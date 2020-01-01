7bde173dbbc9c607b022ef78d41289ea34dc77b4
Ritikri <ritik.1822cs1168@kiet.edu>
2020-01-01 19:33:33 +0530

    Progress bar + Splash Act

--- app/src/main/java/com/example/endeavour/splashAct.java
+++ app/src/main/java/com/example/endeavour/splashAct.java
@@ -2,34 +2,22 @@ package com.example.endeavour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
+
 
 import android.content.Intent;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Pair;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
+
 import android.widget.ImageView;
-import android.widget.TextView;
 
-
-import com.airbnb.lottie.LottieAnimationView;
 
 
 public class splashAct extends AppCompatActivity {
-    private static int timeout=4000;
-   // Button splash_btn;
-    ImageView endlogo;
-  //  ImageView ecelllogo; //added
-    TextView endtext;
-    TextView enddesc;
-    LottieAnimationView animationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,33 +25,14 @@ public class splashAct extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView1);
 
-        //  ecelllogo=(ImageView) findViewById(R.id.imageView1); //added
-        //splash_btn = (Button) findViewById(R.id.btn_splash);
-        endlogo = (ImageView) findViewById(R.id.imageView);
-        endtext = (TextView) findViewById(R.id.textView);
-        enddesc = (TextView) findViewById(R.id.textView2);
-        animationView = (LottieAnimationView) findViewById(R.id.animsplash);
+        final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.zoom_in);
+        final Animation animation_2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.antirotate);
+        //final Animation animation_3 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.zoom_out);
 
-
-        endtext.setAlpha(0.0f);
-        // ecelllogo.setAlpha(0.0f);//added
-        enddesc.setAlpha(0.0f);
-        animationView.setAlpha(0.0f);
-        // splash_btn.setAlpha(0.0f);
-
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.splashanim);
-        endlogo.setAnimation(animation);
-
-        final Animation animationtext1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animlefttoright);
-        // final Animation animation1= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animzoomin2);//added
-        final Animation animationdesc = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animrighttoleft);
-        final Animation animationanim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animzoomin);
-        final Animation animationbtn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animbtnlong);
-
-
-//1
-        animation.setAnimationListener(new Animation.AnimationListener() {
+        imageView.startAnimation(animation_2);
+        animation_2.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
 
@@ -71,62 +40,7 @@ public class splashAct extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                endtext.setAnimation(animationtext1);
-                endtext.setAlpha(1.0f);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-//1_end
-        animationtext1.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-
-                enddesc.setAnimation(animationdesc);
-                enddesc.setAlpha(1.0f);
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-      /*  animationtext1.setAnimationListener(new Animation.AnimationListener() {     //added
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                ecelllogo.setAnimation(animation1);
-                ecelllogo.setAlpha(1.0f);
-           }
-
-        @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });*/
-        animationdesc.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                animationView.setAnimation(animationanim);
-                animationView.setAlpha(1.0f);
-                animationView.playAnimation();
+                imageView.startAnimation(animation_1);
             }
 
             @Override
@@ -135,7 +49,7 @@ public class splashAct extends AppCompatActivity {
             }
         });
 
-      /*  animationanim.setAnimationListener(new Animation.AnimationListener() {
+        animation_1.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
 
@@ -143,29 +57,21 @@ public class splashAct extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                splash_btn.setAnimation(animationbtn);
-                splash_btn.setAlpha(1.0f);
-                animationView.playAnimation();
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });*/
-
-
-        Handler handler=new Handler();  //UI and the background thread COMMUNICATION
-        handler.postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-
-                Intent intent=new Intent(splashAct.this,LoginActivity.class);
-                startActivity(intent);
+                imageView.startAnimation(animation_2);
                 finish();
+                Intent i = new Intent(getBaseContext(),LoginActivity.class);
+                startActivity(i);
+
             }
-        },timeout);
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+
 
     }
 }
