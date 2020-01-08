5db0c02088c24047dbb5be2ae2b88e4d78253e10
Parneet-Raghuvanshi <parnetraghuvanshi@gmail.com>
2020-01-08 20:21:20 +0530

    TEST

--- app/src/main/java/com/example/endeavour/Dashboard.java
+++ app/src/main/java/com/example/endeavour/Dashboard.java
@@ -4,20 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-
-import com.google.firebase.messaging.FirebaseMessaging;
 
 public class Dashboard extends AppCompatActivity {
 
     LinearLayout layoutOurteam;
     LinearLayout layoutevents;
-    ImageView notification_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +22,6 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         layoutevents=(LinearLayout) findViewById(R.id.layout_events);
         layoutOurteam = (LinearLayout) findViewById(R.id.layout_ourteam);
-        notification_btn = (ImageView) findViewById(R.id.iv_notification_btn);
-
-        FirebaseMessaging.getInstance().subscribeToTopic("USER");
 
         layoutevents.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +32,8 @@ public class Dashboard extends AppCompatActivity {
         });
 
 
+
+
         layoutOurteam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,14 +41,6 @@ public class Dashboard extends AppCompatActivity {
                     Intent intent_ourteam = new Intent(Dashboard.this, TeamMain.class);
                     startActivity(intent_ourteam);
                 }
-            }
-        });
-
-        notification_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Dashboard.this,Notifications.class);
-                startActivity(intent);
             }
         });
     }
