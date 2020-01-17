fd683160a51f41546ea4bc284aee6dda661be594
Parneet-Raghuvanshi <parnetraghuvanshi@gmail.com>
2020-01-17 23:08:47 +0530

    errors

--- app/src/main/java/com/example/endeavour/User.java
+++ app/src/main/java/com/example/endeavour/User.java
@@ -2,22 +2,111 @@ package com.example.endeavour;
 
 
 public class User {
-    public String name, email,branch,year,campusid,contactN,CollegeName,uid,refrelid,password;
+    public String name;
+    public String email;
+    public String branch;
+    public String year;
+    public String campusid;
+    public String contactn;
+    public String collegename;
+    public String uid;
+    public String refrelid;
+    public String password;
 
     public User(){
 
     }
 
-    public User(String fname, String email,String branch, String year, String cid,String number, String cname,String uid,String refrelid,String password) {
-        this.name = fname;
+    public User(String name, String email, String branch, String year, String campusid, String contactn, String collegename, String uid, String refrelid, String password) {
+        this.name = name;
         this.email = email;
-        this.branch=branch;
-        this.year=year;
-        this.campusid=cid;
-        this.contactN=number;
-        this.CollegeName=cname;
-        this.uid=uid;
-        this.refrelid=refrelid;
-        this.password=password;
+        this.branch = branch;
+        this.year = year;
+        this.campusid = campusid;
+        this.contactn = contactn;
+        this.collegename = collegename;
+        this.uid = uid;
+        this.refrelid = refrelid;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getCampusid() {
+        return campusid;
+    }
+
+    public void setCampusid(String campusid) {
+        this.campusid = campusid;
+    }
+
+    public String getContactn() {
+        return contactn;
+    }
+
+    public void setContactn(String contactn) {
+        this.contactn = contactn;
+    }
+
+    public String getCollegename() {
+        return collegename;
+    }
+
+    public void setCollegename(String collegename) {
+        this.collegename = collegename;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getRefrelid() {
+        return refrelid;
+    }
+
+    public void setRefrelid(String refrelid) {
+        this.refrelid = refrelid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
